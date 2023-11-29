90be2769e8f0d786f16df35ff49222a86e76f392
yoodahun <dahun4032@gmail.com>
2023-11-29 23:50:17 +0900

    add setupChromeOptions()

--- src/test/java/factory/TestFactory.java
+++ src/test/java/factory/TestFactory.java
@@ -3,6 +3,7 @@ package factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
@@ -13,10 +14,14 @@ public class TestFactory {
     protected WebDriver webdriver;
     protected String platform;
 
-
+    /**
+     * 파라미터를 커맨드라인으로부터 입력받아 초기화합니다.
+     * @param platform sort of platform
+     */
     @Parameters({"platform"})
     @BeforeSuite
     protected void setupParameter(String platform) {
+
         this.platform = platform.toLowerCase();
 
         if ("pc_chrome".equals(this.platform)) {
@@ -27,11 +32,23 @@ public class TestFactory {
 
     @BeforeClass
     protected void setupDriver(){
-        this.webdriver = new ChromeDriver();
+
+        this.webdriver = new ChromeDriver(this.setupChromeOptions());
     }
 
     @AfterClass
     protected void tearDownDriver() {
         this.webdriver.quit();
+    }
+
+    public ChromeOptions setupChromeOptions() {
+
+        ChromeOptions options  = new ChromeOptions();
+
+        options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36");
+        options.addArguments("--start-fullscreen");
+        options.addArguments("--incognito");
+
+        return options;
     }
 }
